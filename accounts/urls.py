d8385dc90805082ad3d906513eba4473a35be1c7
Dishant Rathi <dishant16rathi@gmail.com>
2021-01-30 15:14:26 +0530

    The static files are created

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home),
     path('products/', views.products),
     path('customer/', views.customer),
-]
+] #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- accounts/views.py
+++ accounts/views.py
@@ -6,10 +6,10 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse(request, 'accounts/dashboard.html')
+    return render(request, 'accounts/dashboard.html')
 
 def products(request):
-    return HttpResponse('products')
+    return render(request, 'accounts/products.html')
 
 def customer(request):
-    return HttpResponse('customer')
+    return render(request, 'accounts/customer.html')
