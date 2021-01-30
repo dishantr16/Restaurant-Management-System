2442a6a3fa393b9355e7d9a4e1a7fa9d0078004a
Dishant Rathi <dishant16rathi@gmail.com>
2021-01-30 17:04:28 +0530

    CRUD options inserted in orders and customers

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,10 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.home),
     path('products/', views.products),
-    path('customer/', views.customer),
-] #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('customer/<str:pk_test>/', views.customer, name="customer"),
+
+    path('create_order/<str:pk>/', views.createOrder, name="create_order"),
+    path('update_order/<str:pk>/', views.updateOrder, name="update_order"),
+    path('delete_order/<str:pk>/', views.deleteOrder, name="delete_order"),
+
+] 
